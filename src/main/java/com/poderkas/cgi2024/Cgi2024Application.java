8aaff03c1385cee0654c3a6f26d447a081dd1c60
Kaspar <poder.kaspar@gmail.com>
2024-03-15 20:44:57 +0200

    Initial commit

---
+++ src/main/java/com/poderkas/cgi2024/Cgi2024Application.java
@@ -0,0 +1,13 @@
+package com.poderkas.cgi2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cgi2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/poderkas/cgi2024/Cgi2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poderkas.cgi2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgi2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
